4cf3ea4ced86cc4007781b3981c931d47e32fc9b
SARBRINDER <200354904@student.georgianc.on.ca>
2017-10-02 19:24:18 -0500

    Add project files.

---
+++ Comp2007_Assignment1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2007_Assignment1.Startup))]
+namespace Comp2007_Assignment1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
